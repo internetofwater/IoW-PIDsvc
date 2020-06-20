d10bb3c1341ec2af327598c7d54c160ac5a08f9c
ksonda <44071350+ksonda@users.noreply.github.com>
2020-06-20 18:59:01 -0400

    update docker-compose and dockerfiles
    fixes #6

--- PIDsvc/postgres-init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e 
-
-psql --username "$POSTGRES_USER" -c "create user \"pidsvc-admin\" password 'apassword'"
-createdb --username "$POSTGRES_USER" pidsvc -O pidsvc-admin
-psql --username "$POSTGRES_USER" -d pidsvc -f postgresql.sql
